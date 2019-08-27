38be03e2aebd4f0de24b8f3de1511b7f77130bf6
Max Leitch <mjleitch@gmail.com>
2019-08-26 19:35:24 -0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express'
 
-export function helloWorld(_: Request, res: Response) {
+export function app(_: Request, res: Response) {
     try {
         res.status(200)
         res.send('Hello World')
